5e00e9e3b8105f94033692ca3908a6b2ec73f7cf
Nishant <nishantkumarjha.139@gmail.com>
2023-01-14 18:25:46 +0530

    first occurance in array recursion

---
+++ myfirstproject/src/RECURSIVEBASICS/occurance.java
@@ -0,0 +1,23 @@
+package RECURSIVEBASICS;
+
+public class occurance {
+    public static void main(String args[]){
+      int key=10;
+      int arr[]={1,2,5,6,7,3};
+      int i=0;
+      int position=occurance(key,arr,i);
+      System.out.println(position);
+
+    }
+    public static int occurance(int key,int arr[],int i){
+        if(i==arr.length-1){
+            return -1;
+        }
+        if(key==arr[i]){
+            return i;
+        }
+         
+        return occurance(key,arr,i+1);
+
+    }
+}
